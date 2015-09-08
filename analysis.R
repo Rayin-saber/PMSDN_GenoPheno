1fb187dc60cde591d60328597c7768944006e189
Maxime Wack <maximewack@free.Fr>
2015-09-08 15:26:44 -0400

    AVL-185 Cleaned data management and analyses

--- analysis.R
+++ analysis.R
@@ -1,27 +1,19 @@
 source("functions-deletion.R")
-library(glmnet)
-library(doMC)
 
-registerDoMC(cores = 8)
+load("data.Rda")
 
-load("Phenotypes.Rda")
-load("Genetics.Rda")
-Genetics_ranges <- Genetics
-rm(Genetics)
-
-# Deletions----------------------------------------------------------------------------------
-data <- merge(Demographics, Clinical,             all = T)
-data <- merge(data,         Clinical,             all = T)
-#data <- merge(data,         Clinical_groups,      all = T)
+# ==== Patient selection ====
+patients <- unique(Genetics_ranges$Patient.ID)
+data <- merge(Demographics, Clinical,             by = "Patient.ID")
 data <- merge(data,         Developmental,        all = T)
-#data <- merge(data,         Developmental_groups, all = T)
-
 rm(Demographics, Clinical, Developmental)
 
 vars <- rbind(Clin_vars, Dev_vars)
-
 rm(Clin_vars, Dev_vars)
 
+
+
+# Deletions----------------------------------------------------------------------------------
 # First plot of the deletions
 CNVplot(Genetics_ranges)
 
@@ -72,131 +64,136 @@ for (var in vars2)
 for (var in names(dataroc[-(1:2)]))
   plot(roc(dataroc[[var]], dataroc$Start, percent = T), print.thres = "best", print.auc = T, main = var)
 
-# Lasso---------------------------------------------------------------------------------------
-results_lasso <- vars
-results_lasso[Genes$Gene] <- 0
 
-for (var in vars$Variable)
-{
-  print(var)
-  df <- merge(data[c("Patient.ID", var)], Genetics_genes_bin)
-  df <- df[complete.cases(df),]
-  x <- as.matrix(df[,-(1:2)])
-  multi <- F
-  if (is.numeric(df[[var]]))
-  {
-    cv.fit <- cv.glmnet(x, df[[var]], alpha = .9, parallel = T)
-  } else if (is.ordered(df[[var]]))
-  {
-    cv.fit <- cv.glmnet(x, df[[var]], alpha = .9, parallel = T, family = "multinomial", type.multinomial = "grouped")
-    multi <- T
-  } else
-  {
-    cv.fit <- cv.glmnet(x, df[[var]], alpha = .9, parallel = T, family = "binomial")
-  }
-  plot(cv.fit)
-  if (multi)
-  {
-    lasso <- as.matrix(coef(cv.fit, s = "lambda.min")[[nlevels(df[[var]])]])
-  } else
-  {
-    lasso <- as.matrix(coef(cv.fit, s = "lambda.min"))
-  }
-  results_lasso[results_lasso$Variable == var, -(1:2)] <- lasso[-1]
-}
-
-rm(lasso, x, cv.fit, multi, df)
-
-# GWAS----------------------------------------------------------------------------------
-results_genes_p   <- vars
-results_genes_ICl <- vars
-results_genes_ICu <- vars
-results_genes_OR  <- vars
-
-for (var in vars$Variable)
-{
-  print(var)
-  df <- merge(data, Genetics_genes_bin)
-  for (gene in Genes$Gene)
-  {
-    if (is.numeric(df[[var]]))
-    {
-      model <- lm(df[[var]] ~ df[[gene]] + df$Gender)
-      table <- regress.display(model)$table
-    } else if (is.ordered(df[[var]]))
-    {
-      tryCatch({
-        model <- polr(df[[var]] ~ df[[gene]] + df$Gender + scale(df$Age))
-        table <- ordinal.or.display(model)
-      }, error = function(e) {
-        table <<- NA
-      })
-    } else
-    {
-      if (names(df[var]) == "Is.the.patient's.menstrual.cycle.regular?_ currently")
-        model <- glm(df[[var]] ~ df[[gene]] + df$Age, family = binomial)
-      else
-        model <- glm(df[[var]] ~ df[[gene]] + df$Gender + df$Age, family = binomial)
-      table <- logistic.display(model)$table
-    }
-    tryCatch({
-      results_genes_OR[results_genes_OR$Variable == var, gene]   <- table["df[[gene]]TRUE", 1]
-      results_genes_ICl[results_genes_ICl$Variable == var, gene] <- table["df[[gene]]TRUE", 2]
-      results_genes_ICu[results_genes_ICu$Variable == var, gene] <- table["df[[gene]]TRUE", 3]
-      results_genes_p[results_genes_p$Variable == var, gene]     <- table["df[[gene]]TRUE", 4]
-    }, error = function(e) {
-      results_genes_OR[results_genes_OR$Variable == var, gene]   <<- NA
-      results_genes_ICl[results_genes_ICl$Variable == var, gene] <<- NA
-      results_genes_ICu[results_genes_ICu$Variable == var, gene] <<- NA
-      results_genes_p[results_genes_p$Variable == var, gene]     <<- NA
-    })
-  }
-}
-
-rm(table, model)
-
-dir.create("delplots")
-for (var in vars$Variable)
-  delPlotGenes(Genetics_ranges, data, results_ranges, results_genes_p, results_lasso, Genes, var, noOutput = F)
-
-
-results_genes_mat <- t(apply(results_genes_p[-(1:2)], 1, p.adjust, method = "bonferroni"))
-#results_genes_mat_or <- apply(results_genes_OR[-(1:2)], 2, as.numeric)
-rownames(results_genes_mat) <- results_genes_p$Variable
-#rownames(results_genes_mat_or) <- results_genes_OR$Variable
-#results_genes_mat_or <- results_genes_mat_or[which(apply(results_genes_mat, 1, function(x){min(x) < .05}) == TRUE), which(apply(results_genes_mat, 2, function(x){min(x) < .05}) == TRUE)]
-results_genes_mat <- results_genes_mat[which(apply(results_genes_mat, 1, function(x){min(x) < .05}) == TRUE), which(apply(results_genes_mat, 2, function(x){min(x) < .05}) == TRUE)]
-results_genes_mat <- -log(results_genes_mat, base = 10)
-#results_genes_mat_or <- abs(log(results_genes_mat_or, base = 10))
-rowInd <- heatmap(results_genes_mat, margins = c(7,20), Colv = NA, col = colorRampPalette(c("white","blue"))(100))$rowInd
-#heatmap(results_genes_mat_or[rowInd,], margins = c(7,20), Colv = NA, Rowv = NA, col = colorRampPalette(c("white","blue"))(100))
-
-# Misc-----------------------------------------------------------------------
-lasso <- as.matrix(results_lasso[-(1:2)])
-rownames(lasso) <- results_lasso$Variable
-lasso[lasso != 0] <- 1
-lasso <- lasso * results_genes_mat
-lasso <- 10 ** (-lasso)
-
-lasso <- lasso[apply(lasso, 1, function(x) {any(x < .05, na.rm = T)}), apply(lasso, 2, function(x) {any(x < .05, na.rm = T)})]
-
-library(RCurl)
-
-pubmed <- Genes["Gene"]
-for (gene in Genes$Gene)
-{
-  esearch <- as.character(getURLContent(paste0("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=300&term=",gene)))
-  esearch <- gsub("\\n", "", esearch, perl = T)
-  count <- as.numeric(sub("^.*<eSearchResult><Count>(\\d+)<\\/Count>.*$","\\1", esearch, perl = T))
-  if (count == 0)
-    next
-  esearch <- gsub("^.*<IdList><Id>", "", esearch, perl = T)
-  esearch <- gsub("<\\/Id><\\/IdList>.*$", "", esearch, perl = T)
-  esearch <- gsub("<\\/Id><Id>", ",", esearch, perl = T)
-  pubmed$pmid[pubmed$Gene == gene] <- esearch
-
-  efetch <- as.character(getURLContent(paste0("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&id=",esearch)))
-}
+# library(glmnet)
+# library(doMC)
+#
+# registerDoMC(cores = 8)
+# # Lasso---------------------------------------------------------------------------------------
+# results_lasso <- vars
+# results_lasso[Genes$Gene] <- 0
+# 
+# for (var in vars$Variable)
+# {
+#   print(var)
+#   df <- merge(data[c("Patient.ID", var)], Genetics_genes_bin)
+#   df <- df[complete.cases(df),]
+#   x <- as.matrix(df[,-(1:2)])
+#   multi <- F
+#   if (is.numeric(df[[var]]))
+#   {
+#     cv.fit <- cv.glmnet(x, df[[var]], alpha = .9, parallel = T)
+#   } else if (is.ordered(df[[var]]))
+#   {
+#     cv.fit <- cv.glmnet(x, df[[var]], alpha = .9, parallel = T, family = "multinomial", type.multinomial = "grouped")
+#     multi <- T
+#   } else
+#   {
+#     cv.fit <- cv.glmnet(x, df[[var]], alpha = .9, parallel = T, family = "binomial")
+#   }
+#   plot(cv.fit)
+#   if (multi)
+#   {
+#     lasso <- as.matrix(coef(cv.fit, s = "lambda.min")[[nlevels(df[[var]])]])
+#   } else
+#   {
+#     lasso <- as.matrix(coef(cv.fit, s = "lambda.min"))
+#   }
+#   results_lasso[results_lasso$Variable == var, -(1:2)] <- lasso[-1]
+# }
+# 
+# rm(lasso, x, cv.fit, multi, df)
+# 
+# # GWAS----------------------------------------------------------------------------------
+# results_genes_p   <- vars
+# results_genes_ICl <- vars
+# results_genes_ICu <- vars
+# results_genes_OR  <- vars
+# 
+# for (var in vars$Variable)
+# {
+#   print(var)
+#   df <- merge(data, Genetics_genes_bin)
+#   for (gene in Genes$Gene)
+#   {
+#     if (is.numeric(df[[var]]))
+#     {
+#       model <- lm(df[[var]] ~ df[[gene]] + df$Gender)
+#       table <- regress.display(model)$table
+#     } else if (is.ordered(df[[var]]))
+#     {
+#       tryCatch({
+#         model <- polr(df[[var]] ~ df[[gene]] + df$Gender + scale(df$Age))
+#         table <- ordinal.or.display(model)
+#       }, error = function(e) {
+#         table <<- NA
+#       })
+#     } else
+#     {
+#       if (names(df[var]) == "Is.the.patient's.menstrual.cycle.regular?_ currently")
+#         model <- glm(df[[var]] ~ df[[gene]] + df$Age, family = binomial)
+#       else
+#         model <- glm(df[[var]] ~ df[[gene]] + df$Gender + df$Age, family = binomial)
+#       table <- logistic.display(model)$table
+#     }
+#     tryCatch({
+#       results_genes_OR[results_genes_OR$Variable == var, gene]   <- table["df[[gene]]TRUE", 1]
+#       results_genes_ICl[results_genes_ICl$Variable == var, gene] <- table["df[[gene]]TRUE", 2]
+#       results_genes_ICu[results_genes_ICu$Variable == var, gene] <- table["df[[gene]]TRUE", 3]
+#       results_genes_p[results_genes_p$Variable == var, gene]     <- table["df[[gene]]TRUE", 4]
+#     }, error = function(e) {
+#       results_genes_OR[results_genes_OR$Variable == var, gene]   <<- NA
+#       results_genes_ICl[results_genes_ICl$Variable == var, gene] <<- NA
+#       results_genes_ICu[results_genes_ICu$Variable == var, gene] <<- NA
+#       results_genes_p[results_genes_p$Variable == var, gene]     <<- NA
+#     })
+#   }
+# }
+# 
+# rm(table, model)
+# 
+# dir.create("delplots")
+# for (var in vars$Variable)
+#   delPlotGenes(Genetics_ranges, data, results_ranges, results_genes_p, results_lasso, Genes, var, noOutput = F)
+# 
+# 
+# results_genes_mat <- t(apply(results_genes_p[-(1:2)], 1, p.adjust, method = "bonferroni"))
+# #results_genes_mat_or <- apply(results_genes_OR[-(1:2)], 2, as.numeric)
+# rownames(results_genes_mat) <- results_genes_p$Variable
+# #rownames(results_genes_mat_or) <- results_genes_OR$Variable
+# #results_genes_mat_or <- results_genes_mat_or[which(apply(results_genes_mat, 1, function(x){min(x) < .05}) == TRUE), which(apply(results_genes_mat, 2, function(x){min(x) < .05}) == TRUE)]
+# results_genes_mat <- results_genes_mat[which(apply(results_genes_mat, 1, function(x){min(x) < .05}) == TRUE), which(apply(results_genes_mat, 2, function(x){min(x) < .05}) == TRUE)]
+# results_genes_mat <- -log(results_genes_mat, base = 10)
+# #results_genes_mat_or <- abs(log(results_genes_mat_or, base = 10))
+# rowInd <- heatmap(results_genes_mat, margins = c(7,20), Colv = NA, col = colorRampPalette(c("white","blue"))(100))$rowInd
+# #heatmap(results_genes_mat_or[rowInd,], margins = c(7,20), Colv = NA, Rowv = NA, col = colorRampPalette(c("white","blue"))(100))
+# 
+# # Misc-----------------------------------------------------------------------
+# lasso <- as.matrix(results_lasso[-(1:2)])
+# rownames(lasso) <- results_lasso$Variable
+# lasso[lasso != 0] <- 1
+# lasso <- lasso * results_genes_mat
+# lasso <- 10 ** (-lasso)
+# 
+# lasso <- lasso[apply(lasso, 1, function(x) {any(x < .05, na.rm = T)}), apply(lasso, 2, function(x) {any(x < .05, na.rm = T)})]
+# 
+# library(RCurl)
+# 
+# pubmed <- Genes["Gene"]
+# for (gene in Genes$Gene)
+# {
+#   esearch <- as.character(getURLContent(paste0("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=300&term=",gene)))
+#   esearch <- gsub("\\n", "", esearch, perl = T)
+#   count <- as.numeric(sub("^.*<eSearchResult><Count>(\\d+)<\\/Count>.*$","\\1", esearch, perl = T))
+#   if (count == 0)
+#     next
+#   esearch <- gsub("^.*<IdList><Id>", "", esearch, perl = T)
+#   esearch <- gsub("<\\/Id><\\/IdList>.*$", "", esearch, perl = T)
+#   esearch <- gsub("<\\/Id><Id>", ",", esearch, perl = T)
+#   pubmed$pmid[pubmed$Gene == gene] <- esearch
+# 
+#   efetch <- as.character(getURLContent(paste0("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&id=",esearch)))
+# }
 
 # ==== Pathways ====
 # results_pathways <- rbind(Clin_vars[1:2], Clin_vars_groups[1:2], Dev_vars[1:2], Dev_vars_groups[1:2])
--- data_management.R
+++ data_management.R
@@ -1,42 +1,20 @@
-source("process.R")
+source("functions-process.R")
 
 export_date = as.Date("2015-07-27")
 
+# ==== Demographics ====
 Demographics      <- processDemographics(         noOutput = T)
+
 Demographics$Gender               <- factor(Demographics$Gender)
 Demographics$Ancestral.Background <- factor(Demographics$Ancestral.Background)
 Demographics$Country              <- factor(Demographics$Country)
 
-Clinical          <- processFile("Clinical",      noOutput = T)
-Developmental     <- processFile("Developmental", noOutput = T)
-
-Genetics <- read.csv("Genetics.csv", stringsAsFactors = F)
-
-patients <- intersect(unique(Genetics$Patient.ID),Clinical$Patient.ID)
-Clinical <- Clinical[Clinical$Patient.ID %in% patients,]
-Genetics <- Genetics[Genetics$Patient.ID %in% patients,]
-Developmental <- Developmental[Developmental$Patient.ID %in% patients,]
-Demographics <- Demographics[Demographics$Patient.ID %in% patients,]
-
-# ==== Phenotypic variable selection ====
 # ==== Clinical ====
+Clinical          <- processFile("Clinical",      noOutput = T)
+
 # Preselection
 Clin_vars <- read.csv2("clin_vars.csv",      stringsAsFactors = F)
 Clinical <- Clinical[c("Patient.ID",Clin_vars$Variable)]
-
-# Group creation
-# Clin_vars_groups <- read.csv2("clin_vars_groups.csv", stringsAsFactors = F)
-# Clinical_groups <- select(Clinical, Patient.ID)
-# for (group in Clin_vars_groups$Variable)
-# {
-#   Clinical_groups[group] <- apply(select(Clinical, starts_with(group)), 1, function(x)
-#   {
-#     if (any(x == "Yes"))
-#       "Yes"
-#     else
-#       x[1]
-#   })
-# }
 
 # Data types
 Clinical$`What.was.the.patient's.gestational.age?` <- ifelse(Clinical$`What.was.the.patient's.gestational.age?` == "Over 40 weeks", 41, as.numeric(sub(" weeks", "", Clinical$`What.was.the.patient's.gestational.age?`)))
@@ -51,19 +29,10 @@ Clinical$`What.is.the.highest.number.of.ear.infections.the.patient.has.had.in.a.
 
 for (var in names(Clinical[-1]))
   if (is.character(Clinical[[var]]))
+  {
+    Clinical[var] <- gsub(" (by imputation)", "", Clinical[[var]], fixed = T)
     Clinical[var] <- factor(Clinical[[var]], levels = c("No", "Yes"), exclude = c("", NA))
-
-#for (var in names(Clinical_groups[-1]))
-#  Clinical_groups[var] <- factor(Clinical_groups[[var]], levels = c("No", "Yes"), exclude = c("", NA))
-
-# Summary and selection for groups
-#for (var in Clin_vars_groups$Variable)
-#  for (lvl in levels(Clinical_groups[[var]]))
-#    if (!is.na(lvl))
-#      Clin_vars_groups[Clin_vars_groups$Variable == var, lvl] <- summary(factor(Clinical_groups[[var]]))[lvl]
-
-#Clin_vars_groups <- filter(Clin_vars_groups, Yes > 5, No > 5)
-#Clinical_groups <- Clinical_groups[c("Patient.ID", Clin_vars_groups$Variable)]
+  }
 
 # Summary and selection for individual vars
 for (var in names(Clinical[-1]))
@@ -80,23 +49,11 @@ for (var in Clin_vars$Variable)
 write.csv2(Clin_vars, "clin_vars_summary.csv")
 
 # ==== Developmental ====
+Developmental     <- processFile("Developmental", noOutput = T)
+
 # Preselection
 Dev_vars  <- read.csv2("dev_vars.csv",       stringsAsFactors = F)
 Developmental <- Developmental[c("Patient.ID", Dev_vars$Variable)]
-
-# Group creation
-#Dev_vars_groups <- read.csv2("dev_vars_groups.csv", stringsAsFactors = F)
-#Developmental_groups <- select(Developmental, Patient.ID)
-#for (group in Dev_vars_groups$Variable)
-#{
-#  Developmental_groups[group] <- apply(select(Developmental, starts_with(group)), 1, function(x)
-#  {
-#    if (any(x == "Yes"))
-#      "Yes"
-#    else
-#      x[1]
-#  })
-#}
 
 # Data types
 for (var in names(Developmental))
@@ -105,11 +62,11 @@ for (var in names(Developmental))
 
 
 for (var in grep("(communication|motor)\\.milestones", names(Developmental), value = T))
-  Developmental[var] <- factor(Developmental[[var]], levels = c("0 - 3 months", "4  - 7 months",	"8 - 11 months",	"12 - 18 months",	"19 - 24 months",	"25 - 30 months",	"31 - 36 months", "4 - 5 years", "6 - 7 years",	"8 - 9 years", "10 years +"), ordered = T)
+  Developmental[var] <- factor(Developmental[[var]], levels = c("0 - 3 months", "4 - 7 months",	"8 - 11 months",	"12 - 18 months",	"19 - 24 months",	"25 - 30 months",	"31 - 36 months", "4 - 5 years", "6 - 7 years",	"8 - 9 years", "10 years +"), ordered = T)
 
 for (var in grep("(communication|motor)\\.milestones", names(Developmental), value = T))
 {
-  levels <- c("0 - 3 months", "4  - 7 months",	"8 - 11 months",	"12 - 18 months",	"19 - 24 months",	"25 - 30 months",	"31 - 36 months", "4 - 5 years", "6 - 7 years",	"8 - 9 years", "10 years +")
+  levels <- c("0 - 3 months", "4 - 7 months",	"8 - 11 months",	"12 - 18 months",	"19 - 24 months",	"25 - 30 months",	"31 - 36 months", "4 - 5 years", "6 - 7 years",	"8 - 9 years", "10 years +")
   unclassed <- unclass(Developmental[[var]])
   lvl_low <- 1
   while (sum(summary(Developmental[[var]])[1:lvl_low]) < 6 & lvl_low < 11)
@@ -148,18 +105,10 @@ Developmental$`Is.the.patient.able.to.feed.himself/herself.independently?_1_Answ
 
 for (var in names(Developmental[-1]))
   if (is.character(Developmental[[var]]))
+  {
+    Developmental[var] <- gsub(" (by imputation)", "", Developmental[[var]], fixed = T)
     Developmental[var] <- factor(Developmental[[var]], levels = c("No", "Yes"), exclude = c("", NA))
-
-#for (var in names(Developmental_groups[-1]))
-#  if (is.character(Developmental_groups[[var]]))
-#    Developmental_groups[var] <- factor(Developmental_groups[[var]], levels = c("No", "Yes"), exclude = c("", NA))
-
-#for (var in Dev_vars_groups$Variable)
-#  for (lvl in levels(Developmental_groups[[var]]))
-#    if (!is.na(lvl))
-#      Dev_vars_groups[Dev_vars_groups$Variable == var, lvl] <- summary(factor(Developmental_groups[[var]]))[lvl]
-#Dev_vars_groups <- filter(Dev_vars_groups, Yes > 5, No > 5)
-#Developmental_groups <- Developmental_groups[c("Patient.ID", Dev_vars_groups$Variable)]
+  }
 
 # Summary and selection for individual vars
 for (var in names(Developmental[-1]))
@@ -175,47 +124,14 @@ for (var in Dev_vars$Variable)
       Dev_vars[Dev_vars$Variable == var, lvl] <- summary(factor(Developmental[[var]]))[lvl]
 write.csv2(Dev_vars, "dev_vars_summary.csv")
 
-
-Clin_vars <- select(Clin_vars, Group, Variable)
-#Clin_vars_groups <- select(Clin_vars_groups, Group, Variable)
-Dev_vars <- select(Dev_vars, Group, Variable)
-#Dev_vars_groups <- select(Dev_vars_groups, Group, Variable)
-save(Clin_vars, Dev_vars, Clinical, Developmental, Demographics, file = "Phenotypes.Rda")
-
 # ==== Genetics ====
-Genetics_ranges    <- processRanges(Genetics)
-Genetics_genes     <- processGenes(Genetics)
-Genetics_genes_bin <- processGenes(Genetics, bin = T)
-Genetics_pathways  <- processPathways(Genetics, Genetics_genes)
+Genetics_ranges <- processGenetics(noOutput = T)
 
-# Genes modified in at least 5 patients
-Genes <- data.frame(t(apply(Genetics_genes_bin[-(1:2)], 2, function(x){summary(factor(x))[1:2]})))
-Genes <- add_rownames(Genes, var = "Gene")
-Genes <- filter(Genes, TRUE. > 5, FALSE. > 5)
-Genetics_genes <- select(Genetics_genes, Patient.ID, one_of(Genes$Gene))
-Genetics_genes_bin <- select(Genetics_genes_bin, Patient.ID, one_of(Genes$Gene))
+Genetics_ranges$Start <- as.numeric(Genetics_ranges$Start)
+Genetics_ranges$End   <- as.numeric(Genetics_ranges$End)
 
-# Pathways modified in at least 5 patients
-Genetics_pathways_bin <- data.frame(apply(Genetics_pathways, 2, function(x){x > 0}))
-names(Genetics_pathways_bin) <- names(Genetics_pathways)
-Genetics_pathways_bin$Patient.ID <- Genetics_pathways$Patient.ID
-Pathways <- data.frame(t(apply(Genetics_pathways_bin[-1], 2, function(x){summary(factor(x))})))
-Pathways <- add_rownames(Pathways, var = "Pathway")
-Pathways <- filter(Pathways, TRUE. > 5, FALSE. > 5)
-Genetics_pathways <- select(Genetics_pathways, Patient.ID, one_of(Pathways$Pathway))
-Genetics_pathways_bin <- select(Genetics_pathways_bin, Patient.ID, one_of(Pathways$Pathway))
+# ==== Export ====
+Clin_vars <- select(Clin_vars, Group, Variable)
+Dev_vars <- select(Dev_vars, Group, Variable)
 
-# Reorder genes based on position on chr22
-hg38 <- read.delim("refGene.txt.hg38", stringsAsFactors = F)
-hg38 <- filter(hg38, name2 %in% Genes$Gene, chrom == "chr22") %>% select(chrom, txStart, txEnd, name2) %>% arrange(name2) %>% distinct %>% group_by(name2) %>% summarise(txStart = min(txStart), txEnd = max(txEnd))
-Genes <- merge(Genes, hg38, by.x = "Gene", by.y = "name2") %>% arrange(txStart)
-Genetics_genes_bin <- select(Genetics_genes_bin, Patient.ID, one_of(Genes$Gene))
-
-Genetics_genes_bin$Patient.ID    <- as.character(Genetics_genes_bin$Patient.ID)
-Genetics_pathways$Patient.ID     <- as.character(Genetics_pathways$Patient.ID)
-Genetics_pathways_bin$Patient.ID <- as.character(Genetics_pathways_bin$Patient.ID)
-Genetics_ranges$Patient.ID       <- as.character(Genetics_ranges$Patient.ID)
-Genetics_ranges$Start            <- as.numeric(Genetics_ranges$Start)
-Genetics_ranges$End              <- as.numeric(Genetics_ranges$End)
-
-save(Genes, Genetics_genes_bin, Genetics_pathways, Genetics_pathways_bin, Genetics_ranges, file = "Genetics.Rda")
+save(Clin_vars, Dev_vars, Clinical, Developmental, Demographics, Genetics_ranges, file = "data.Rda")
