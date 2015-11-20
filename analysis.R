07dcceddaa2554b0379ab36a5d0bcea923b60f6d
Maxime Wack <maximewack@free.Fr>
2015-11-20 19:35:28 +0100

    Code refactoring and cleaning. Extensive use of Hadley's packages for readability

--- analysis.R
+++ analysis.R
@@ -1,27 +1,113 @@
-source("functions-deletion.R")
+library(dplyr)
+library(ggplot2)
+library(magrittr)
+library(cowplot)
+library(pROC)
+library(parallel)
+library(RSvgDevice)
+
+#source("functions-deletion.R")
 
 load("data.Rda")
 
-# ==== Patient selection ====
-patients <- unique(Genetics_ranges$Patient.ID)
-data <- merge(Demographics, Clinical,             all = T)
-data <- merge(data,         Developmental,        all = T)
-rm(Demographics, Clinical, Developmental)
+# Create main data frame -------------------------------------------------------
+full_join(Demographics, Clinical) %>%
+  full_join(Developmental) -> data
 
 vars <- rbind(Clin_vars, Dev_vars)
+
+rm(Demographics, Clinical, Developmental)
 rm(Clin_vars, Dev_vars)
 
-# Deletions----------------------------------------------------------------------------------
-# First plot of the deletions
-CNVplot(Genetics_ranges)
+# Manage Genetics_ranges -------------------------------------------------------
+Genetics_ranges %<>%
+  filter(Chr_Gene == "22") %>%
+  mutate(Patient.ID = as.character(Patient.ID)) %>%
+  mutate(Gain_Loss = ifelse(Result.type == "mutation", "Mutation", Gain_Loss)) %>%
+  select(-Result.type, -Chr_Gene)
 
-# Keep only terminal deletions/mutations on chr22
-Genetics_ranges <- filter(Genetics_ranges, Chr_Gene == "22", Gain_Loss != "Gain", End > 50500000) # shank3 = 50674641
-data <- filter(data, Patient.ID %in% Genetics_ranges$Patient.ID)
+# Create the del extend var ----------------------------------------------------
+Genetics_ranges %<>%
+  full_join(
+    Genetics_ranges %>%
+      filter(Gain_Loss != "Gain") %>%
+      group_by(Patient.ID) %>%
+      summarize(min = min(Start))
+  ) %>%
+  arrange(desc(min))
 
-# Second plot of the deletions
-CNVplot(Genetics_ranges)
+# First plot with all patients and every genetic alteration --------------------
+Genetics_ranges %>%
+  mutate(End = ifelse(Gain_Loss == "Mutation", End + 5e4, End)) %>%
+  ggplot(aes(x = Patient.ID, ymin = Start/1e6, ymax = End/1e6)) +
+  geom_linerange(size = 1, aes(color = Gain_Loss)) +
+  scale_color_manual(values = c(Gain = "blue", Loss = "red", mutation = "darkred")) +
+  scale_x_discrete(labels = NULL, limits = unique(Genetics_ranges$Patient.ID[order(desc(Genetics_ranges$min))])) +
+  theme(axis.ticks.y = element_blank(), legend.position = "left") +
+  ylab("Chromosomic position") +
+  xlab("Patients") +
+  geom_rect(ymin = 50674641/1e6, ymax = 50733212/1e6, xmin = 1, xmax = nrow(Genetics_ranges), alpha = .01, fill = "grey") +
+  coord_flip() -> CNV_plot
 
+# Keep only terminal deletions/mutations ---------------------------------------
+Genetics_ranges %<>% filter(Gain_Loss != "Gain", End > 50500000) # shank3 = 50674641
+
+# And only patients who have phenotypic data ----------------------------------
+Genetics_ranges %<>% semi_join(data)
+
+# Re-compute del extent var ----------------------------------------------------
+Genetics_ranges %<>%
+  full_join(
+    Genetics_ranges %>%
+      group_by(Patient.ID) %>%
+      summarize(min = min(Start))
+  ) %>%
+  arrange(desc(min))
+
+# Final plot of the deletions for the included patients ------------------------
+Genetics_ranges %>%
+  mutate(End = ifelse(Gain_Loss == "Mutation", End + 5e4, End)) %>%
+  ggplot(aes(x = Patient.ID, ymin = Start/1e6, ymax = End/1e6)) +
+  geom_linerange(size = 1, aes(color = Gain_Loss)) +
+  scale_color_manual(values = c(Loss = "red", mutation = "darkred")) +
+  scale_x_discrete(labels = NULL, limits = unique(Genetics_ranges$Patient.ID[order(desc(Genetics_ranges$min))])) +
+  theme(axis.ticks.y = element_blank(), axis.line.y = element_blank(), legend.position = "left", legend.title = element_blank()) +
+  ylab("Chromosomic position") +
+  xlab("Patients") +
+  geom_rect(ymin = 50674641/1e6, ymax = 50733212/1e6, xmin = 0.5, xmax = nrow(Genetics_ranges), alpha = .01, fill = "grey") +
+  coord_flip() -> DEL_plot
+
+# Keep only patients with geno & pheno data and build dataframe ----------------
+data %<>%
+  inner_join(
+    Genetics_ranges %>%
+      distinct(Patient.ID) %>%
+      select(Patient.ID, min)
+  ) %>%
+  arrange(desc(min))
+
+# Plot for one var (test)-------------------------------------------------------
+data %>%
+  ggplot(aes(x = Patient.ID, ymin = 0, ymax = 1, color = data[["Has.the.patient.been.diagnosed.with.any.of.the.following.kidney.conditions?_Vesicoureteral Reflux (Renal reflux)_Conditions"]])) +#`Has.the.patient.been.diagnosed.with.any.of.the.following.kidney.conditions?_Vesicoureteral Reflux (Renal reflux)_Conditions`)) +
+  geom_linerange(size = 1) +
+  scale_x_discrete(labels = NULL, limits = data$Patient.ID[order(desc(data$min))]) +
+  scale_y_continuous(labels = NULL) +
+  scale_color_grey(start = .9, end = .2, na.value = "white") +
+  theme(axis.ticks = element_blank(), axis.line = element_blank(), legend.position = "none") +
+  xlab("") +
+  ggtitle("Vesico-ureteral Reflux") +
+  theme(plot.title = element_text(angle = 45, vjust = 0.5, hjust = 0.5, size = 10)) +
+  coord_flip() -> g
+
+plot_grid(DEL_plot, g, g, g, g, align = "h", nrow = 1, rel_widths = c(4, 1, 1, 1, 1)) %>%
+  save_plot(filename = "test.svg", device = devSVG, nrow = 1, base_height = 7, base_width = 12)
+
+
+# Association analysis ---------------------------------------------------------
+
+data %>%
+  select(-Patient.ID, Birthdate, Gender, Ancestral.Background, Country) %>%
+  lapply(summary)
 results_ranges <- vars
 results_ranges$Range_p <- NA
 results_ranges$ICl <- NA
@@ -37,7 +123,7 @@ results_ranges$Range_p_corrected <- p.adjust(results_ranges$Range_p, "fdr")
 results_ranges <- arrange(results_ranges, Range_p_corrected)
 write.csv2(results_ranges, "results_ranges.csv")
 
-# Plots--------------------------------------------------------------------------
+# Plots-------------------------------------------------------------------------
 dir.create("delplotsrangesvg")
 for (var in vars$Variable[1:100])
   delPlotRange(Genetics_ranges, data, results_ranges, var, noOutput = T, bnw = T)
@@ -48,38 +134,53 @@ for (var in vars$Variable[vars$Group == "Renal.-.Kidney"])
 for (var in vars$Variable[vars$Group == "Gross.Motor.Development"])
   delPlotRange(Genetics_ranges, data, results_ranges, var, noOutput = T, bnw = T)
 
-# ROC curves---------------------------------------------------------------------
+# ROC curves--------------------------------------------------------------------
 # Keep only the maximum extent of deletion for each patient
-library(pROC)
 Start <- Genetics_ranges %>% group_by(Patient.ID) %>% summarize(Start = min(Start))
-rocvars <- results_ranges[as.numeric(results_ranges$Range_p_corrected) < .05, c("Group", "Variable")]
+rocvars <- filter(results_ranges, Range_p_corrected < .05) %>% select(Group, Variable)
 data$Patient.ID <- as.numeric(data$Patient.ID)
-dataroc <- merge(Start,data[c("Patient.ID",rocvars$Variable)])
+datarocs <- merge(Start,data[c("Patient.ID",rocvars$Variable)])
 for (var in rocvars$Variable)
-  if (nlevels(dataroc[[var]]) > 2)
-    dataroc[var] <- NULL
+  if (nlevels(datarocs[[var]]) > 2 | nlevels(datarocs[[var]]) == 0)
+    datarocs[var] <- NULL
 
-rocvars <- rocvars[rocvars$Variable %in% names(dataroc)[-(1:2)],]
+rocvars <- rocvars[rocvars$Variable %in% names(datarocs)[-(1:2)],]
 
 for (var in rocvars$Variable)
 {
+  print(var)
+  dataroc <- select(datarocs, one_of("Start", var))
+  thres_bt <- unlist(mclapply(1:10000, mc.cores = 8, function(x)
+  {
+    dataroc_sample <- sample_n(dataroc, nrow(dataroc), replace = T)
+    roc <- tryCatch({roc(dataroc_sample[[var]], dataroc_sample$Start, percent = T)}, error = function(e){roc(dataroc[[var]], dataroc$Start, percent = T)})
+
+    youden <- roc$sensitivities + roc$specificities - 100
+    roc$thresholds[which(youden == max(youden))]
+  }))
+
   roc <- roc(dataroc[[var]], dataroc$Start, percent = T)
-  plot(roc, print.thres = "best", print.auc = T, main = var)
-  
   youden <- roc$sensitivities + roc$specificities - 100
-  plot(youden)
   thres <- roc$thresholds[which(youden == max(youden))]
-  
-  rocvars$min[rocvars$Variable == var] <- max(dataroc$Start[dataroc$Start<thres])
-  rocvars$max[rocvars$Variable == var] <- min(dataroc$Start[dataroc$Start>thres])
+
+  hist(thres_bt, main = var, freq = F, xlim = c(40e6, 51e6))
+  lines(density(thres_bt, adjust = 2))
+  abline(v = mean(thres_bt), col = "orange")
+  abline(v = thres, col = "darkorange", lty = 2)
+  abline(v = quantile(thres_bt, probs = .025), col = "green")
+  abline(v = quantile(thres_bt, probs = .975), col = "red")
+
+
+  rocvars$min[rocvars$Variable == var] <- max(dataroc$Start[dataroc$Start < thres])
+  rocvars$max[rocvars$Variable == var] <- min(dataroc$Start[dataroc$Start > thres])
   rocvars$auc[rocvars$Variable == var] <- as.numeric(auc(roc))
+  rocvars$thres[rocvars$Variable == var] <- thres
+  rocvars$icl[rocvars$Variable == var] <- quantile(thres_bt, probs = .025)
+  rocvars$icu[rocvars$Variable == var] <- quantile(thres_bt, probs = .975)
 }
 
 # rocvars <- rocvars[rocvars$auc > 70,]
-
-library(ggplot2)
-
-# write.csv(rocvars, "rocvars.csv")
+write.csv(rocvars, "rocvars.csv")
 rocvars <- read.csv("rocvars.csv")
 rocvars <- merge(rocvars, group_by(rocvars, Group) %>% summarise(gmin = min(min)))
 rocvars <- merge(rocvars, group_by(rocvars, Group) %>% summarise(gmax = max(max)))
@@ -87,185 +188,10 @@ rocvars <- arrange(rocvars, gmin, Group,min)
 rocvars$Variable <- factor(rocvars$Variable, levels = rocvars$Variable)
 rocvars$Group <- factor(rocvars$Group, levels = rev(unique(rocvars$Group)))
 
-ggplot(rocvars) + 
-  geom_linerange(aes(x=Variable, ymin = min/1000000, ymax = max/1000000, colour = Group), size = 8) +
-  geom_linerange(aes(x=Variable, ymin = gmin/1000000, ymax = gmax/1000000, colour = Group), size = 8, alpha = .5) +
-  geom_text(aes(x=Variable, y = gmax/1000000, label = paste0(as.integer(auc), "%")), hjust=0) +
+ggplot(rocvars) +
+  geom_linerange(aes(x = Variable, ymin = min/1000000, ymax = max/1000000, colour = Group), size = 8) +
+  geom_linerange(aes(x = Variable, ymin = icl/1e6, ymax = icu/1e6, colour = Group), size = 8, alpha = .5) +
+  #geom_linerange(aes(x=Variable, ymin = gmin/1000000, ymax = gmax/1000000, colour = Group), size = 8, alpha = .5) +
+  geom_text(aes(x = Variable, y = gmax/1000000, label = paste0(as.integer(auc), "%")), hjust=0) +
   scale_y_continuous(name = "Position") +
   coord_flip()
-
-# library(glmnet)
-# library(doMC)
-#
-# registerDoMC(cores = 8)
-# # Lasso---------------------------------------------------------------------------------------
-# results_lasso <- vars
-# results_lasso[Genes$Gene] <- 0
-# 
-# for (var in vars$Variable)
-# {
-#   print(var)
-#   df <- merge(data[c("Patient.ID", var)], Genetics_genes_bin)
-#   df <- df[complete.cases(df),]
-#   x <- as.matrix(df[,-(1:2)])
-#   multi <- F
-#   if (is.numeric(df[[var]]))
-#   {
-#     cv.fit <- cv.glmnet(x, df[[var]], alpha = .9, parallel = T)
-#   } else if (is.ordered(df[[var]]))
-#   {
-#     cv.fit <- cv.glmnet(x, df[[var]], alpha = .9, parallel = T, family = "multinomial", type.multinomial = "grouped")
-#     multi <- T
-#   } else
-#   {
-#     cv.fit <- cv.glmnet(x, df[[var]], alpha = .9, parallel = T, family = "binomial")
-#   }
-#   plot(cv.fit)
-#   if (multi)
-#   {
-#     lasso <- as.matrix(coef(cv.fit, s = "lambda.min")[[nlevels(df[[var]])]])
-#   } else
-#   {
-#     lasso <- as.matrix(coef(cv.fit, s = "lambda.min"))
-#   }
-#   results_lasso[results_lasso$Variable == var, -(1:2)] <- lasso[-1]
-# }
-# 
-# rm(lasso, x, cv.fit, multi, df)
-# 
-# # GWAS----------------------------------------------------------------------------------
-# results_genes_p   <- vars
-# results_genes_ICl <- vars
-# results_genes_ICu <- vars
-# results_genes_OR  <- vars
-# 
-# for (var in vars$Variable)
-# {
-#   print(var)
-#   df <- merge(data, Genetics_genes_bin)
-#   for (gene in Genes$Gene)
-#   {
-#     if (is.numeric(df[[var]]))
-#     {
-#       model <- lm(df[[var]] ~ df[[gene]] + df$Gender)
-#       table <- regress.display(model)$table
-#     } else if (is.ordered(df[[var]]))
-#     {
-#       tryCatch({
-#         model <- polr(df[[var]] ~ df[[gene]] + df$Gender + scale(df$Age))
-#         table <- ordinal.or.display(model)
-#       }, error = function(e) {
-#         table <<- NA
-#       })
-#     } else
-#     {
-#       if (names(df[var]) == "Is.the.patient's.menstrual.cycle.regular?_ currently")
-#         model <- glm(df[[var]] ~ df[[gene]] + df$Age, family = binomial)
-#       else
-#         model <- glm(df[[var]] ~ df[[gene]] + df$Gender + df$Age, family = binomial)
-#       table <- logistic.display(model)$table
-#     }
-#     tryCatch({
-#       results_genes_OR[results_genes_OR$Variable == var, gene]   <- table["df[[gene]]TRUE", 1]
-#       results_genes_ICl[results_genes_ICl$Variable == var, gene] <- table["df[[gene]]TRUE", 2]
-#       results_genes_ICu[results_genes_ICu$Variable == var, gene] <- table["df[[gene]]TRUE", 3]
-#       results_genes_p[results_genes_p$Variable == var, gene]     <- table["df[[gene]]TRUE", 4]
-#     }, error = function(e) {
-#       results_genes_OR[results_genes_OR$Variable == var, gene]   <<- NA
-#       results_genes_ICl[results_genes_ICl$Variable == var, gene] <<- NA
-#       results_genes_ICu[results_genes_ICu$Variable == var, gene] <<- NA
-#       results_genes_p[results_genes_p$Variable == var, gene]     <<- NA
-#     })
-#   }
-# }
-# 
-# rm(table, model)
-# 
-# dir.create("delplots")
-# for (var in vars$Variable)
-#   delPlotGenes(Genetics_ranges, data, results_ranges, results_genes_p, results_lasso, Genes, var, noOutput = F)
-# 
-# 
-# results_genes_mat <- t(apply(results_genes_p[-(1:2)], 1, p.adjust, method = "bonferroni"))
-# #results_genes_mat_or <- apply(results_genes_OR[-(1:2)], 2, as.numeric)
-# rownames(results_genes_mat) <- results_genes_p$Variable
-# #rownames(results_genes_mat_or) <- results_genes_OR$Variable
-# #results_genes_mat_or <- results_genes_mat_or[which(apply(results_genes_mat, 1, function(x){min(x) < .05}) == TRUE), which(apply(results_genes_mat, 2, function(x){min(x) < .05}) == TRUE)]
-# results_genes_mat <- results_genes_mat[which(apply(results_genes_mat, 1, function(x){min(x) < .05}) == TRUE), which(apply(results_genes_mat, 2, function(x){min(x) < .05}) == TRUE)]
-# results_genes_mat <- -log(results_genes_mat, base = 10)
-# #results_genes_mat_or <- abs(log(results_genes_mat_or, base = 10))
-# rowInd <- heatmap(results_genes_mat, margins = c(7,20), Colv = NA, col = colorRampPalette(c("white","blue"))(100))$rowInd
-# #heatmap(results_genes_mat_or[rowInd,], margins = c(7,20), Colv = NA, Rowv = NA, col = colorRampPalette(c("white","blue"))(100))
-# 
-# # Misc-----------------------------------------------------------------------
-# lasso <- as.matrix(results_lasso[-(1:2)])
-# rownames(lasso) <- results_lasso$Variable
-# lasso[lasso != 0] <- 1
-# lasso <- lasso * results_genes_mat
-# lasso <- 10 ** (-lasso)
-# 
-# lasso <- lasso[apply(lasso, 1, function(x) {any(x < .05, na.rm = T)}), apply(lasso, 2, function(x) {any(x < .05, na.rm = T)})]
-# 
-# library(RCurl)
-# 
-# pubmed <- Genes["Gene"]
-# for (gene in Genes$Gene)
-# {
-#   esearch <- as.character(getURLContent(paste0("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=300&term=",gene)))
-#   esearch <- gsub("\\n", "", esearch, perl = T)
-#   count <- as.numeric(sub("^.*<eSearchResult><Count>(\\d+)<\\/Count>.*$","\\1", esearch, perl = T))
-#   if (count == 0)
-#     next
-#   esearch <- gsub("^.*<IdList><Id>", "", esearch, perl = T)
-#   esearch <- gsub("<\\/Id><\\/IdList>.*$", "", esearch, perl = T)
-#   esearch <- gsub("<\\/Id><Id>", ",", esearch, perl = T)
-#   pubmed$pmid[pubmed$Gene == gene] <- esearch
-# 
-#   efetch <- as.character(getURLContent(paste0("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&id=",esearch)))
-# }
-
-# ==== Pathways ====
-# results_pathways <- rbind(Clin_vars[1:2], Clin_vars_groups[1:2], Dev_vars[1:2], Dev_vars_groups[1:2])
-# for (var in Clin_vars$Variable)
-# {
-#   print(var)
-#   for (pathway in Pathways$Pathway)
-#   {
-#     results_pathways[results_pathways$Variable == var, pathway] <- fisher.test(factor(Clinical[[var]], exclude = c("", NA)), factor(Genetics_pathways_bin[[pathway]]), workspace = 1e8)$p.value
-#   }
-# }
-#
-# for (var in Clin_vars_groups$Variable)
-# {
-#   print(var)
-#   for (pathway in Pathways$Pathway)
-#   {
-#     results_pathways[results_pathways$Variable == var, pathway] <- fisher.test(factor(Clinical_groups[[var]], exclude = c("", NA)), factor(Genetics_pathways_bin[[pathway]]), workspace = 1e8)$p.value
-#   }
-# }
-#
-# Genetics_pathways_bin <- filter(Genetics_pathways_bin, Patient.ID %in% Developmental$Patient.ID)
-# for (var in Dev_vars$Variable)
-# {
-#   print(var)
-#   for (pathway in Pathways$Pathway)
-#   {
-#     results_pathways[results_pathways$Variable == var, pathway] <- fisher.test(factor(Developmental[[var]], exclude = c("", NA)), factor(Genetics_pathways_bin[[pathway]]), workspace = 1e8)$p.value
-#   }
-# }
-#
-# for (var in Dev_vars_groups$Variable)
-# {
-#   print(var)
-#   for (pathway in Pathways$Pathway)
-#   {
-#     results_pathways[results_pathways$Variable == var, pathway] <- fisher.test(factor(Developmental_groups[[var]], exclude = c("", NA)), factor(Genetics_pathways_bin[[pathway]]), workspace = 1e8)$p.value
-#   }
-# }
-#
-# results_pathways_mat <- apply(results_pathways[-(1:2)], 2, p.adjust, method = "bonferroni")
-# #results_pathways_mat <- t(apply(results_pathways_mat, 1, p.adjust, method = "fdr"))
-# rownames(results_pathways_mat) <- results_pathways$Variable
-# results_pathways_mat <- results_pathways_mat[which(apply(results_pathways_mat, 1, function(x){min(x) < .05}) == TRUE), which(apply(results_pathways_mat, 2, function(x){min(x) < .05}) == TRUE)]
-# results_pathways_mat <- -log(results_pathways_mat)
-# heatmap(results_pathways_mat, col = colorRampPalette(c("white","blue"))(100))
