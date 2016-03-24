14c903b786802b2324ccfed4a2e2c2da15ee477b
Maxime Wack <maximewack@free.Fr>
2016-03-24 02:58:13 +0100

    Added variables renaming for aesthetic purposes in plots

--- analysis.R
+++ analysis.R
@@ -38,8 +38,43 @@ vars %>%
             mutate(Group = Group %>% factor)) %>%
   mutate(text = text %>% sub("\\(.*?\\)$", "", ., perl = T)) %>%
   mutate(text = text %>% gsub("\\.", " ", ., perl = T)) %>%
-  mutate(text = text %>% sub("With PMS, some children develop understandable verbal speech while others do not  If the patient is over age 2, please choose the response that most closely matches your child's abilities today", "Verbal speech", ., perl = T)) %>%
-  mutate(text = text %>% sub("Has the patient ever exhibited symptoms of or been diagnosed with ADD/ADHD or any other significant attention or hyperactivity issues warranting intervention?", "Symptoms/diagnostic of ADD/ADHD", ., perl = T)) -> vars
+  mutate(text = text %>% sub("With PMS, some children develop understandable verbal speech while others do not  If the patient is over age 2, please choose the response that most closely matches your child's abilities today", "Verbal speech", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Has the patient ever exhibited symptoms of or been diagnosed with ADD/ADHD or any other significant attention or hyperactivity issues warranting intervention?", "Symptoms/diagnostic of ADD/ADHD", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Has less post-operative pain than would be expected", "Less post-op pain than expected", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Needs a great deal of post-operative pain medication", "Great need of post-op pain med", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Needs less post-operative pain medication than would be expected", "Less post-op pain med than expected", ., fixed = T)) %>%
+  mutate(text = text %>% sub("What is the highest number of ear infections the patient has had in a given year?", "Highest number of ear infections in a year", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Has the patient had ear infections after age three, which required recurrent courses of antibiotics?", "Ear infections age>3, requiring antibiotics", ., fixed = T)) %>%
+  mutate(text = text %>% sub("If the patient ever had a metabolic panel test, was it abnormal?", "Abnormal metabolic panel test", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Excessive irritability especially following mealtimes", "Excessive irritability following mealtimes", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Two or more serious sinus infections within one year", "2+ serious sinus infections within a year", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Two or more months on antibiotics with little effect", "2+ months on antibiotics with little effect", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Persistent thrush in mouth or fungal infection on skin", "Persistent thrush in mouth/fungal infection on skin", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Need for intravenous antibiotics to clear infections", "Need for IV antibiotics to clear infections", ., fixed = T)) %>%
+  mutate(text = text %>% sub("If not, has the delay resulted from immune system concerns?", "Delay resulted from immune system concerns", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Has the patient ever had a severe adverse reaction to any vaccines?", "Severe adverse reaction to any vaccines", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Bloating is apparent at all times but varies in severity", "Bloating apparent at all times but varies in severity", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Concerns about the mother's ability to deliver vaginally", "Concerns about the mother's ability to deliver vaginally", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Difficulty going back to sleep after nighttime awakening", "Difficulty going back to sleep after nighttime awakening", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Have difficulty sustaining attention in tasks or play activities", "Difficulty sustaining attention in tasks", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Leave seat in classroom or in other situations in which remaining seated is expected", "Leave seat when remaining seated is expected", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Run around or climb excessively in situations in which it is inappropriate", "Run around or climb excessively when inappropriate", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Have difficulty playing or engaging in leisure activities quietly", "Difficulty playing quietly", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Has the patient ever exhibited excessive mouthing/chewing not associated with teething?", "Excessive chewing not associated with teething", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Demonstrate understanding of the use of familiar objects", "Understanding of the use of familiar objects", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Act out recent experiences using gestures, body languages, words", "Act out recent experiences using gestures, etc.", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Observe actions of others and imitate them at a later time", "Observe actions and imitate them later", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Has the patient ever experienced regression in cognitive abilities?", "Regression in cognitive abilities", ., fixed = T)) %>%
+  mutate(text = text %>% sub("What is the patient's ability to follow directions?", "Ability to follow directions", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Look at, reach and grasp objects placed at a distance", "Look at, reach and grasp distant objects", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Does the patient have difficulties with motor planning or motor learning?", "Difficulties with motor planning or learning", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Does the patient have difficulties maintaining balance?", "Difficulties maintaining balance", ., fixed = T)) %>%
+  mutate(text = text %>% sub("If the patient has difficulties maintaining balance, does the inability to balance depend on sight?", "Inability to balance dependant on sight", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Mixed hypo- and hyper-sensitivity to touch throughout the body", "Mixed hypo- and hyper-sensitivity to touch", ., fixed = T)) %>%
+  mutate(text = text %>% sub("What is the level of patient's response to movement?", "Response to movement", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Initiate social interaction by smiling, moving arms and/or vocalizing", "Initiate social interaction", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Has the patient ever had poor eye contact?", "Poor eye contact", ., fixed = T)) %>%
+  mutate(text = text %>% sub("Cling to caregivers/familiar adult in presence of a stranger", "Cling to familiar adult in presence of a stranger", ., fixed = T)) -> vars
 
 # Manage Genetics_ranges -------------------------------------------------------
 Genetics_ranges %<>%
