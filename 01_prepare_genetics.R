6d8227eb76c1af6985136545f5e8ab8449af3cbf
Maxime Wack <maximewack@free.Fr>
2019-02-25 06:41:03 +0100

    Added automated mapping to hg38

---
+++ 01_prepare_genetics.R
@@ -0,0 +1,132 @@
+library(tidyverse)
+
+# liftOver can produce multiple overlapping output.
+# This function merges ranges to their simplest form possible
+merge_overlapping_genetic_ranges <- function(ranges)
+{
+  ranges %>%
+    gather(end, pos, hg38.Start, hg38.End) %>%
+    arrange(pos) %>%
+    mutate(depth = cumsum(end == "hg38.Start") -
+           cumsum(end == "hg38.End") %>%
+           lag %>%
+           replace_na(0)) %>%
+    filter(depth == 1) %>%
+    mutate(num = cumsum(end == "hg38.Start")) %>%
+    spread(end, pos) %>%
+    select(rowname, num, hg38.Start, hg38.End)
+}
+
+# Write the liftOver.bed file on disk, for the provided data
+write_bedfile <- function(data)
+{
+  data %>%
+    mutate(Chr = "chr22",
+           id = str_c(Patient.ID, "_", Array.num)) %>%
+  select(Chr, Array.Start, Array.End, id) %>%
+  write.table(file = "liftOver.bed" , sep = "\t", row.names = F, col.names = F, quote = F)
+}
+
+# Execute liftOver in two passes.
+# - with unique mappings
+# - with multiple outputs on the firt failed ones, and merging
+liftOver <- function(build, data)
+{
+  chainfile = str_c(build, "ToHg38.over.chain")
+
+# First try with unique mapping
+  data %>%
+    write_bedfile
+
+  system2("./liftOver", c("liftOver.bed", chainfile, "liftOver.out", "liftOver.unmap"))
+
+  read_tsv("liftOver.out", col_names = c("Chr", "hg38.Start", "hg38.End", "rowname")) -> out
+  if (length(out) > 0)
+  {
+    out %>%
+      separate(rowname, into = c("Patient.ID", "Array.num"), sep = "_") %>%
+      mutate_at(vars(Patient.ID, Array.num), as.numeric) %>%
+      select(-Chr) -> out
+  }
+
+# Second liftOver pass with multiple alignment and auto merging
+  read_tsv("liftOver.unmap", col_names = c("Chr", "Array.Start", "Array.End", "rowname"), comment = "#") -> unmap
+  if (length(unmap) > 0)
+  {
+    data %>%
+      anti_join(out) %>%
+      write_bedfile
+
+    system2("./liftOver", c("liftOver.bed", chainfile, "liftOver.out", "liftOver.unmap", "-multiple"))
+
+    read_tsv("liftOver.out", col_names = c("Chr", "hg38.Start", "hg38.End", "rowname", "num")) -> outunmap
+    if (length(outunmap) > 0)
+    {
+      outunmap %>%
+        group_by(rowname) %>%
+        do(merge_overlapping_genetic_ranges(.)) %>%
+        separate(rowname, into = c("Patient.ID", "Array.num"), sep = "_") %>%
+        mutate_at(vars(Patient.ID, Array.num), as.numeric) %>%
+        bind_rows(out) -> out
+    }
+  }
+
+  out
+}
+
+# Process each row according to the result type
+process <- function(type, data)
+{
+  if (type == "Array")
+  {
+    data %>%
+      group_by(Array.BrowserBuild) %>%
+      nest %>%
+      mutate(liftover = map2(Array.BrowserBuild, data, liftOver),
+             data2 = map2(data, liftover, left_join)) %>%
+      unnest(data2) %>%
+      # Compare with pre-filled mappings and replace
+      mutate(startok = Array.Start.latest == hg38.Start,
+             endok = Array.End.latest == hg38.End) %>%
+      arrange(Patient.ID, Array.num) %>%
+      mutate(Array.Start.latest = case_when(Array.Start.latest %>% is.na & hg38.Start %>% is.na ~ NA_real_,
+                                            Array.Start.latest %>% is.na ~ hg38.Start,
+                                            hg38.Start %>% is.na ~ Array.Start.latest,
+                                            T ~ hg38.Start),
+             Array.End.latest = case_when(Array.End.latest %>% is.na & hg38.End %>% is.na ~ NA_real_,
+                                          Array.End.latest %>% is.na ~ hg38.End,
+                                          hg38.End %>% is.na ~ Array.End.latest,
+                                          T ~ hg38.End)) %>%
+      select(-hg38.Start, -hg38.End, -startok, -endok) %>%
+      mutate(num = (num - 1) %>% replace_na(0),
+             Array.num = Array.num + num) %>%
+      select(-num)
+  } else
+  {
+    data
+  }
+}
+
+# Process all the genetic results
+read_csv("data/genetic.csv") %>%
+  mutate(Array.BrowserBuild = Array.BrowserBuild %>% fct_recode(hg17 = "NCBI35/hg17",
+                                                                hg18 = "NCBI36/hg18",
+                                                                hg19 = "GRCh37/hg19")) %>%
+  group_by(Result.type) %>%
+  nest %>%
+  mutate(data = map2(Result.type, data, process)) %>%
+  unnest -> genetic
+
+# Write the processed genetic results
+genetic %>%
+  write_csv("data/genetic_hg38.csv")
+
+
+# Plot check
+genetic %>%
+  filter(Result.type == "Array") %>%
+  mutate(Patient.ID = Patient.ID %>% factor %>% fct_reorder(Array.Start.latest, min)) %>%
+  ggplot() +
+  aes(x = Patient.ID, ymin = Array.Start.latest, ymax = Array.End.latest, color = Array.Type) +
+  geom_linerange() +
+  coord_flip()
