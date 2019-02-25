76c379b9e82e5b9aff232f964734e32d29f9aec0
Maxime Wack <maximewack@free.Fr>
2019-02-25 01:55:59 +0100

    Added script to download liftOver and chain files

---
+++ 00_opt_download_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O liftOver http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/liftOver
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz -O - | gunzip > hg19ToHg38.over.chain
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg18/liftOver/hg18ToHg38.over.chain.gz -O - | gunzip > hg18ToHg38.over.chain
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg17/liftOver/hg17ToHg19.over.chain.gz -O - | gunzip > hg17ToHg19.over.chain
