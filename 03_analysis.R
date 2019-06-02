4ff8243f843d964f15e28960d22a10a7dd36174e
Maxime Wack <maximewack@free.Fr>
2019-06-03 00:09:23 +0200

    Comment part with ROC curves

--- 03_analysis.R
+++ 03_analysis.R
@@ -213,66 +213,66 @@ article$results_ranges %>%
 
 saveRDS(article, "article.rds")
 
-# ROC curves--------------------------------------------------------------------
-# Keep only the maximum extent of deletion for each patient
-Genetics_ranges %>%
-  group_by(Patient.ID) %>%
-  summarize(Start = min(Start)) -> Start
-rocvars <- filter(results_ranges, Range_p_corrected < .05) %>% select(Group, Variable)
-data$Patient.ID <- as.numeric(data$Patient.ID)
-datarocs <- merge(Start,data[c("Patient.ID",rocvars$Variable)])
-for (var in rocvars$Variable)
-  if (nlevels(datarocs[[var]]) > 2 | nlevels(datarocs[[var]]) == 0)
-    datarocs[var] <- NULL
+## ROC curves--------------------------------------------------------------------
+## Keep only the maximum extent of deletion for each patient
+#Genetics_ranges %>%
+#  group_by(Patient.ID) %>%
+#  summarize(Start = min(Start)) -> Start
+#rocvars <- filter(results_ranges, Range_p_corrected < .05) %>% select(Group, Variable)
+#data$Patient.ID <- as.numeric(data$Patient.ID)
+#datarocs <- merge(Start,data[c("Patient.ID",rocvars$Variable)])
+#for (var in rocvars$Variable)
+#  if (nlevels(datarocs[[var]]) > 2 | nlevels(datarocs[[var]]) == 0)
+#    datarocs[var] <- NULL
 
-rocvars <- rocvars[rocvars$Variable %in% names(datarocs)[-(1:2)],]
+#rocvars <- rocvars[rocvars$Variable %in% names(datarocs)[-(1:2)],]
 
-for (var in rocvars$Variable)
-{
-  print(var)
-  dataroc <- select(datarocs, one_of("Start", var))
-  thres_bt <- unlist(mclapply(1:10000, mc.cores = 8, function(x)
-  {
-    dataroc_sample <- sample_n(dataroc, nrow(dataroc), replace = T)
-    roc <- tryCatch({roc(dataroc_sample[[var]], dataroc_sample$Start, percent = T)}, error = function(e){roc(dataroc[[var]], dataroc$Start, percent = T)})
+#for (var in rocvars$Variable)
+#{
+#  print(var)
+#  dataroc <- select(datarocs, one_of("Start", var))
+#  thres_bt <- unlist(mclapply(1:10000, mc.cores = 8, function(x)
+#  {
+#    dataroc_sample <- sample_n(dataroc, nrow(dataroc), replace = T)
+#    roc <- tryCatch({roc(dataroc_sample[[var]], dataroc_sample$Start, percent = T)}, error = function(e){roc(dataroc[[var]], dataroc$Start, percent = T)})
 
-    youden <- roc$sensitivities + roc$specificities - 100
-    roc$thresholds[which(youden == max(youden))]
-  }))
+#    youden <- roc$sensitivities + roc$specificities - 100
+#    roc$thresholds[which(youden == max(youden))]
+#  }))
 
-  roc <- roc(dataroc[[var]], dataroc$Start, percent = T)
-  youden <- roc$sensitivities + roc$specificities - 100
-  thres <- roc$thresholds[which(youden == max(youden))]
+#  roc <- roc(dataroc[[var]], dataroc$Start, percent = T)
+#  youden <- roc$sensitivities + roc$specificities - 100
+#  thres <- roc$thresholds[which(youden == max(youden))]
 
-  hist(thres_bt, main = var, freq = F, xlim = c(40e6, 51e6))
-  lines(density(thres_bt, adjust = 2))
-  abline(v = mean(thres_bt), col = "orange")
-  abline(v = thres, col = "darkorange", lty = 2)
-  abline(v = quantile(thres_bt, probs = .025), col = "green")
-  abline(v = quantile(thres_bt, probs = .975), col = "red")
+#  hist(thres_bt, main = var, freq = F, xlim = c(40e6, 51e6))
+#  lines(density(thres_bt, adjust = 2))
+#  abline(v = mean(thres_bt), col = "orange")
+#  abline(v = thres, col = "darkorange", lty = 2)
+#  abline(v = quantile(thres_bt, probs = .025), col = "green")
+#  abline(v = quantile(thres_bt, probs = .975), col = "red")
 
 
-  rocvars$min[rocvars$Variable == var] <- max(dataroc$Start[dataroc$Start < thres])
-  rocvars$max[rocvars$Variable == var] <- min(dataroc$Start[dataroc$Start > thres])
-  rocvars$auc[rocvars$Variable == var] <- as.numeric(auc(roc))
-  rocvars$thres[rocvars$Variable == var] <- thres
-  rocvars$icl[rocvars$Variable == var] <- quantile(thres_bt, probs = .025)
-  rocvars$icu[rocvars$Variable == var] <- quantile(thres_bt, probs = .975)
-}
+#  rocvars$min[rocvars$Variable == var] <- max(dataroc$Start[dataroc$Start < thres])
+#  rocvars$max[rocvars$Variable == var] <- min(dataroc$Start[dataroc$Start > thres])
+#  rocvars$auc[rocvars$Variable == var] <- as.numeric(auc(roc))
+#  rocvars$thres[rocvars$Variable == var] <- thres
+#  rocvars$icl[rocvars$Variable == var] <- quantile(thres_bt, probs = .025)
+#  rocvars$icu[rocvars$Variable == var] <- quantile(thres_bt, probs = .975)
+#}
 
-# rocvars <- rocvars[rocvars$auc > 70,]
-write.csv(rocvars, "rocvars.csv")
-rocvars <- read.csv("rocvars.csv")
-rocvars <- merge(rocvars, group_by(rocvars, Group) %>% summarise(gmin = min(min)))
-rocvars <- merge(rocvars, group_by(rocvars, Group) %>% summarise(gmax = max(max)))
-rocvars <- arrange(rocvars, gmin, Group,min)
-rocvars$Variable <- factor(rocvars$Variable, levels = rocvars$Variable)
-rocvars$Group <- factor(rocvars$Group, levels = rev(unique(rocvars$Group)))
+## rocvars <- rocvars[rocvars$auc > 70,]
+#write.csv(rocvars, "rocvars.csv")
+#rocvars <- read.csv("rocvars.csv")
+#rocvars <- merge(rocvars, group_by(rocvars, Group) %>% summarise(gmin = min(min)))
+#rocvars <- merge(rocvars, group_by(rocvars, Group) %>% summarise(gmax = max(max)))
+#rocvars <- arrange(rocvars, gmin, Group,min)
+#rocvars$Variable <- factor(rocvars$Variable, levels = rocvars$Variable)
+#rocvars$Group <- factor(rocvars$Group, levels = rev(unique(rocvars$Group)))
 
-ggplot(rocvars) +
-  geom_linerange(aes(x = Variable, ymin = min / 1000000, ymax = max / 1000000, colour = Group), size = 8) +
-  geom_linerange(aes(x = Variable, ymin = icl / 1e6, ymax = icu / 1e6, colour = Group), size = 8, alpha = .5) +
-  #geom_linerange(aes(x=Variable, ymin = gmin/1000000, ymax = gmax/1000000, colour = Group), size = 8, alpha = .5) +
-  geom_text(aes(x = Variable, y = gmax / 1000000, label = srt_c(as.integer(auc), "%")), hjust = 0) +
-  scale_y_continuous(name = "Position") +
-  coord_flip()
+#ggplot(rocvars) +
+#  geom_linerange(aes(x = Variable, ymin = min / 1000000, ymax = max / 1000000, colour = Group), size = 8) +
+#  geom_linerange(aes(x = Variable, ymin = icl / 1e6, ymax = icu / 1e6, colour = Group), size = 8, alpha = .5) +
+#  #geom_linerange(aes(x=Variable, ymin = gmin/1000000, ymax = gmax/1000000, colour = Group), size = 8, alpha = .5) +
+#  geom_text(aes(x = Variable, y = gmax / 1000000, label = srt_c(as.integer(auc), "%")), hjust = 0) +
+#  scale_y_continuous(name = "Position") +
+#  coord_flip()
